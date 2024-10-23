d2d6049aaacd5f77d12ebc67655b1e220531886a
kapildev9494 <kapildev9494@users.noreply.github.com>
2024-10-23 03:25:09 +0000

    chore: update design tokens

---
+++ src/theme/theme.ts
@@ -0,0 +1,279 @@
+import { Theme } from '@fluentui/react-components';
+
+interface BrandThemeExtension {
+  // borderRadiusNone tokens
+  'borderRadiusNone': string;
+
+  // borderRadiusSmall tokens
+  'borderRadiusSmall': string;
+
+  // borderRadiusMedium tokens
+  'borderRadiusMedium': string;
+
+  // borderRadiusLarge tokens
+  'borderRadiusLarge': string;
+
+  // borderRadiusXLarge tokens
+  'borderRadiusXLarge': string;
+
+  // borderRadiusCircular tokens
+  'borderRadiusCircular': string;
+
+  // fontSize tokens
+  'fontSize0': string;
+  'fontSize1': string;
+  'fontSize2': string;
+  'fontSize3': string;
+  'fontSize4': string;
+  'fontSize5': string;
+  'fontSize6': string;
+  'fontSize7': string;
+  'fontSize8': string;
+  'fontSize9': string;
+  'fontSize10': string;
+  'fontSize1000': string;
+
+  // fontSizeBase tokens
+  'fontSizeBase100': string;
+  'fontSizeBase200': string;
+  'fontSizeBase300': string;
+  'fontSizeBase400': string;
+  'fontSizeBase500': string;
+  'fontSizeBase600': string;
+  'fontSizeBase700': string;
+  'fontSizeBase800': string;
+  'fontSizeBase900': string;
+  'fontSizeBase980': string;
+  'fontSizeBase920': string;
+
+  // lineHeightBase tokens
+  'lineHeightBase100': string;
+  'lineHeightBase200': string;
+  'lineHeightBase300': string;
+  'lineHeightBase400': string;
+  'lineHeightBase500': string;
+  'lineHeightBase600': string;
+  'lineHeightBase700': string;
+  'lineHeightBase800': string;
+  'lineHeightBase900': string;
+  'lineHeightBase920': string;
+  'lineHeightBase980': string;
+
+  // lineHeightbase tokens
+  'lineHeightbase1000': string;
+
+  // fontFamilyBase tokens
+  'fontFamilyBase': string;
+
+  // fontWeightRegular tokens
+  'fontWeightRegular': string;
+
+  // fontWeightSemibold tokens
+  'fontWeightSemibold': string;
+
+  // fontWeightBold tokens
+  'fontWeightBold': string;
+
+  // fontWeightSemilight tokens
+  'fontWeightSemilight': string;
+
+  // fontWeightLight tokens
+  'fontWeightLight': string;
+
+  // strokeWidthNone tokens
+  'strokeWidthNone': string;
+
+  // strokeWidthThin tokens
+  'strokeWidthThin': string;
+
+  // strokeWidthThick tokens
+  'strokeWidthThick': string;
+
+  // strokeWidthThicker tokens
+  'strokeWidthThicker': string;
+
+  // strokeWidthThickest tokens
+  'strokeWidthThickest': string;
+
+  // spacingHorizontalNone tokens
+  'spacingHorizontalNone': string;
+
+  // spacingHorizontalXXS tokens
+  'spacingHorizontalXXS': string;
+
+  // spacingHorizontalXS tokens
+  'spacingHorizontalXS': string;
+
+  // spacingHorizontalSNudge tokens
+  'spacingHorizontalSNudge': string;
+
+  // spacingHorizontalS tokens
+  'spacingHorizontalS': string;
+
+  // spacingHorizontalMNudge tokens
+  'spacingHorizontalMNudge': string;
+
+  // spacingHorizontalM tokens
+  'spacingHorizontalM': string;
+
+  // spacingHorizontalL tokens
+  'spacingHorizontalL': string;
+
+  // spacingHorizontalXL tokens
+  'spacingHorizontalXL': string;
+
+  // spacingHorizontalXXL tokens
+  'spacingHorizontalXXL': string;
+
+  // spacingHorizontalXXXL tokens
+  'spacingHorizontalXXXL': string;
+
+  // spacingHorizontalLNudge tokens
+  'spacingHorizontalLNudge': string;
+
+  // spacingHorizontalXXXXL tokens
+  'spacingHorizontalXXXXL': string;
+
+  // spacingVerticalNone tokens
+  'spacingVerticalNone': string;
+
+  // spacingVerticalXXS tokens
+  'spacingVerticalXXS': string;
+
+  // spacingVerticalXS tokens
+  'spacingVerticalXS': string;
+
+  // spacingVerticalSNudge tokens
+  'spacingVerticalSNudge': string;
+
+  // spacingVerticalS tokens
+  'spacingVerticalS': string;
+
+  // spacingVerticalMNudge tokens
+  'spacingVerticalMNudge': string;
+
+  // spacingVerticalM tokens
+  'spacingVerticalM': string;
+
+  // spacingVerticalL tokens
+  'spacingVerticalL': string;
+
+  // spacingVerticalXL tokens
+  'spacingVerticalXL': string;
+
+  // spacingVerticalXXL tokens
+  'spacingVerticalXXL': string;
+
+  // spacingVerticalXXXL tokens
+  'spacingVerticalXXXL': string;
+
+  // spacingVerticalLNudge tokens
+  'spacingVerticalLNudge': string;
+
+  // spacingVerticalXXXXL tokens
+  'spacingVerticalXXXXL': string;
+}
+
+export const brandTheme: Theme & BrandThemeExtension = {
+  'borderRadiusNone': '0px',
+  'borderRadiusSmall': '2px',
+  'borderRadiusMedium': '4px',
+  'borderRadiusLarge': '6px',
+  'borderRadiusXLarge': '8px',
+  'borderRadiusCircular': '9999px',
+  'fontSize0': '10px',
+  'fontSize1': '12px',
+  'fontSize2': '14px',
+  'fontSize3': '16px',
+  'fontSize4': '20px',
+  'fontSize5': '24px',
+  'fontSize6': '28px',
+  'fontSize7': '32px',
+  'fontSize8': '40px',
+  'fontSize9': '48px',
+  'fontSize10': '52px',
+  'fontSize1000': '68px',
+  'fontSizeBase100': '10px',
+  'fontSizeBase200': '12px',
+  'fontSizeBase300': '14px',
+  'fontSizeBase400': '16px',
+  'fontSizeBase500': '20px',
+  'fontSizeBase600': '24px',
+  'fontSizeBase700': '28px',
+  'fontSizeBase800': '32px',
+  'fontSizeBase900': '40px',
+  'fontSizeBase980': '52px',
+  'fontSizeBase920': '48px',
+  'lineHeightBase100': '14px',
+  'lineHeightBase200': '16px',
+  'lineHeightBase300': '22px',
+  'lineHeightBase400': '24px',
+  'lineHeightBase500': '28px',
+  'lineHeightBase600': '34px',
+  'lineHeightBase700': '36px',
+  'lineHeightBase800': '48px',
+  'lineHeightBase900': '52px',
+  'lineHeightbase1000': '92px',
+  'lineHeightBase920': '64px',
+  'lineHeightBase980': '70px',
+  'fontFamilyBase': 'Segoe UI',
+  'fontWeightRegular': '400',
+  'fontWeightSemibold': '600',
+  'fontWeightBold': '700',
+  'fontWeightSemilight': '300',
+  'fontWeightLight': '200',
+  'strokeWidthNone': '0px',
+  'strokeWidthThin': '1px',
+  'strokeWidthThick': '2px',
+  'strokeWidthThicker': '4px',
+  'strokeWidthThickest': '6px',
+  'spacingHorizontalNone': '0px',
+  'spacingHorizontalXXS': '2px',
+  'spacingHorizontalXS': '4px',
+  'spacingHorizontalSNudge': '6px',
+  'spacingHorizontalS': '8px',
+  'spacingHorizontalMNudge': '12px',
+  'spacingHorizontalM': '16px',
+  'spacingHorizontalL': '32px',
+  'spacingHorizontalXL': '48px',
+  'spacingHorizontalXXL': '52px',
+  'spacingHorizontalXXXL': '56px',
+  'spacingHorizontalLNudge': '24px',
+  'spacingHorizontalXXXXL': '64px',
+  'spacingVerticalNone': '0px',
+  'spacingVerticalXXS': '2px',
+  'spacingVerticalXS': '4px',
+  'spacingVerticalSNudge': '6px',
+  'spacingVerticalS': '8px',
+  'spacingVerticalMNudge': '12px',
+  'spacingVerticalM': '16px',
+  'spacingVerticalL': '32px',
+  'spacingVerticalXL': '48px',
+  'spacingVerticalXXL': '52px',
+  'spacingVerticalXXXL': '56px',
+  'spacingVerticalLNudge': '24px',
+  'spacingVerticalXXXXL': '64px'
+};
+
+// Utility functions
+export const convertHexToRgba = (hex: string): string => {
+  if (hex === 'transparent') return 'rgba(0, 0, 0, 0)';
+  if (hex.startsWith('rgba')) return hex;
+  
+  const hexColor = hex.replace('#', '');
+  const r = parseInt(hexColor.substring(0, 2), 16);
+  const g = parseInt(hexColor.substring(2, 4), 16);
+  const b = parseInt(hexColor.substring(4, 6), 16);
+  const a = hexColor.length === 8 ? parseInt(hexColor.substring(6, 8), 16) / 255 : 1;
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+};
+
+// Theme tokens object
+export const brandThemeTokens = Object.entries(brandTheme).reduce((acc, [key, value]) => {
+  if (value.startsWith('#')) {
+    acc[key] = convertHexToRgba(value);
+  } else {
+    acc[key] = value;
+  }
+  return acc;
+}, {} as Record<string, string>);
